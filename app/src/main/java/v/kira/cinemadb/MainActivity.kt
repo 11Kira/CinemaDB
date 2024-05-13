eaa12151ee5774b622aaae4ced6c0ba24c06f4b5
kira <kiravillagracia@yahoo.com>
2024-05-13 16:09:20 +0800

    working passing of data arguments to navigation

--- app/src/main/java/v/kira/cinemadb/MainActivity.kt
+++ app/src/main/java/v/kira/cinemadb/MainActivity.kt
@@ -23,19 +23,24 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
+import v.kira.cinemadb.Graph.DETAILS_SCREEN_ROUTE
 import v.kira.cinemadb.features.details.DetailsScreen
 import v.kira.cinemadb.features.movies.MovieListScreen
 import v.kira.cinemadb.features.navigation.AccountScreen
 import v.kira.cinemadb.features.navigation.BottomMenuItem
 import v.kira.cinemadb.features.navigation.SearchScreen
 import v.kira.cinemadb.features.tv.TVShowListScreen
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 
 @AndroidEntryPoint
@@ -117,29 +122,51 @@ fun BottomNavigation(navController: NavController) {
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
-    NavHost(navController, startDestination = BottomMenuItem.Home.screenRoute) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomMenuItem.Home.screenRoute
+    ) {
         composable(BottomMenuItem.Home.screenRoute) {
-            MovieListScreen(onItemClick = { movieId, movieImage ->
-                navController.navigate("details/$movieId/$movieImage")
-            })
+            MovieListScreen(
+                onItemClick = { movieId, movieImage ->
+                    val encodedMovieImage = URLEncoder.encode(movieImage, StandardCharsets.UTF_8.toString())
+                    navController.navigate("${Graph.DETAILS_GRAPH}/${movieId}/${encodedMovieImage}")
+                }
+            )
         }
         composable(BottomMenuItem.TV.screenRoute) { TVShowListScreen() }
         composable(BottomMenuItem.Search.screenRoute) { SearchScreen() }
         composable(BottomMenuItem.Account.screenRoute) { AccountScreen() }
+        detailsNavGraph(navController = navController)
+    }
+}
+
+fun NavGraphBuilder.detailsNavGraph(navController: NavHostController) {
+
+    navigation(
+        route = "${Graph.DETAILS_GRAPH}/{movieId}/{movieImage}",
+        startDestination = DETAILS_SCREEN_ROUTE,
+    ) {
         composable(
-            route = "details/{movieId}/{movieImage}",
+            route = DETAILS_SCREEN_ROUTE,
             arguments = listOf(
                 navArgument("movieId") {
-                    type = NavType.LongType
+                    type = NavType.StringType
                 },
                 navArgument("movieImage") {
                     type = NavType.StringType
                 }
             )
         ) {
-            val movieId = it.arguments?.getLong("movieId") ?: 0
+            val movieId = it.arguments?.getString("movieId")?.toLong() ?: 0L
             val movieImage = it.arguments?.getString("movieImage") ?: ""
             DetailsScreen(movieId = movieId, movieImage = movieImage)
         }
     }
 }
+
+object Graph {
+    const val DETAILS_GRAPH = "details_graph"
+    const val DETAILS_SCREEN_ROUTE = "details/{movieId}/{movieImage}"
+}
+
