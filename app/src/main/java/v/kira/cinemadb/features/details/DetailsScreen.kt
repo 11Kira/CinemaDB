a3dcd082a70a3e105ad1299414e7cd043fece23f
kira <kiravillagracia@yahoo.com>
2024-05-17 17:17:21 +0800

    added some details

--- app/src/main/java/v/kira/cinemadb/features/details/DetailsScreen.kt
+++ app/src/main/java/v/kira/cinemadb/features/details/DetailsScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,10 +19,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
@@ -69,13 +77,8 @@ fun MainScreen(sharedFlow: SharedFlow<DetailsState>) {
         }
     }
 
-    selectedMovie?.let {
-        SetupMovieDetails(movie = it)
-    }
-
-    selectedTVShow?.let {
-        SetupTVShowDetails(tvShow = it)
-    }
+    selectedMovie?.let { SetupMovieDetails(movie = it) }
+    selectedTVShow?.let { SetupTVShowDetails(tvShow = it) }
 }
 
 @Composable
@@ -116,6 +119,48 @@ fun SetupTVShowDetails(tvShow: TVShowResult) {
                 .wrapContentHeight()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = tvShow.name)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .navigationBarsPadding()
+                .padding(top = 10.dp, start = 10.dp, bottom = 10.dp)
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 50.dp,
+                        topEnd = 0.dp,
+                        bottomStart = 0.dp,
+                        bottomEnd = 50.dp
+                    )
+                )
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF2D01FE),
+                            Color(
+                                0xFF8E3DFD
+                            )
+                        )
+                    ),
+                    shape = RoundedCornerShape(topStart = 30.dp, bottomEnd = 30.dp)
+                )
+                .padding(40.dp)
+        ) {
+            Text(
+                textAlign = TextAlign.Center,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth(),
+                text = tvShow.name,
+                color = Color.White
+            )
+            Text(
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.fillMaxWidth(),
+                text = tvShow.overview,
+                color = Color.White
+            )
+        }
     }
 }
