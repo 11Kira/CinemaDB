53244ed9589585bdffe3bbb1cc2414d0a8c243b0
kirav <kiravillagracia@yahoo.com>
2024-05-28 21:05:01 +0800

    added tv paging source classes

---
+++ app/src/main/java/v/kira/cinemadb/features/tv/TrendingTVShowPagingSource.kt
@@ -0,0 +1,31 @@
+package v.kira.cinemadb.features.tv
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import v.kira.cinemadb.model.TVShowResult
+
+class TrendingTVShowPagingSource(
+    private val header: String,
+    private val language: String,
+    private val remoteSource: TVRemoteSource
+): PagingSource<Int, TVShowResult>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TVShowResult> {
+        return try {
+            val currentPage = params.key ?: 1
+            val response = remoteSource.getTrendingTVShows(header, language, page = currentPage)
+            val movies = response.body()?.results.orEmpty()
+
+            LoadResult.Page(
+                data = movies,
+                prevKey = if (currentPage == 1) null else currentPage - 1,
+                nextKey = if (currentPage < response.body()?.totalPages!!) currentPage + 1 else null
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, TVShowResult>): Int? {
+        return null
+    }
+}
