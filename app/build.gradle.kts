40d111102807879ae1054534b28b229b07ec5e0e
k.i.r.a. <kiravillagracia@yahoo.com>
2026-02-05 13:59:38 +0800

    added dependencies for type safe navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -90,6 +91,7 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.material3)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
