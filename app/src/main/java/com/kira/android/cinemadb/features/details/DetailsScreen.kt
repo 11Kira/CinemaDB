8123e9a979f5befa678f6069e874619ab620728f
k.i.r.a. <kiravillagracia@yahoo.com>
2026-02-09 08:08:39 +0800

    updated cast list to be displayed to 7

--- app/src/main/java/com/kira/android/cinemadb/features/details/DetailsScreen.kt
+++ app/src/main/java/com/kira/android/cinemadb/features/details/DetailsScreen.kt
@@ -193,7 +193,7 @@ fun SetupMovieDetails(movie: MovieResult) {
 
                 val casts = ArrayList<String>()
                 movie.credits?.cast?.forEach { cast ->
-                    if (casts.size < 6) casts.add(cast.name)
+                    if (casts.size < 7) casts.add(cast.name)
                 }
                 Text(
                     textAlign = TextAlign.Start,
@@ -395,7 +395,7 @@ fun SetupTVShowDetails(tvShow: TVShowResult) {
 
                 val casts = ArrayList<String>()
                 tvShow.credits?.cast?.forEach { cast ->
-                    if (casts.size < 6) casts.add(cast.name)
+                    if (casts.size < 7) casts.add(cast.name)
                 }
 
                 Text(
